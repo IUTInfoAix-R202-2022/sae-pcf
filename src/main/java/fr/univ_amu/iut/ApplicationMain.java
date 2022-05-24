60982736a60b2c2d994e077f647246adb9a6ade4
MariusPisto <marius.pistoresi@gmail.com>
2022-05-24 10:23:57 +0200

    Home back side in development

--- src/main/java/fr/univ_amu/iut/ApplicationMain.java
+++ src/main/java/fr/univ_amu/iut/ApplicationMain.java
@@ -1,6 +1,9 @@
 package fr.univ_amu.iut;
 
+import fr.univ_amu.iut.windows.Home;
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 public class ApplicationMain extends Application {
@@ -11,6 +14,9 @@ public class ApplicationMain extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-
+        Pane root = new Home();
+        Scene s = new Scene(root);
+        primaryStage.setScene(s);
+        primaryStage.show();
     }
 }
