0903e4080d9dc89ad1454939b4f543b220b3772d
MariusPisto <marius.pistoresi@gmail.com>
2022-05-24 09:26:48 +0200

    Created ApplicationMain.java

---
+++ src/main/java/fr/univ_amu/iut/ApplicationMain.java
@@ -0,0 +1,16 @@
+package fr.univ_amu.iut;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class ApplicationMain extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
