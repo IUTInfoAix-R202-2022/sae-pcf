7e232fa1a85ad169347065b5d634fc69dab5a856
MariusPisto <marius.pistoresi@gmail.com>
2022-05-25 14:38:36 +0200

    ApplicationMain.java in preparation to work with all classes

--- src/main/java/fr/univ_amu/iut/ApplicationMain.java
+++ src/main/java/fr/univ_amu/iut/ApplicationMain.java
@@ -5,23 +5,39 @@ import javafx.scene.layout.Pane;
 import fr.univ_amu.iut.windows.MainWindow;
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class ApplicationMain extends Application {
+
+    private Scene root;
 
     public static void main(String[] args) {
         launch(args);
     }
 
     @Override
-    public void start(Stage primaryStage) {
-        primaryStage.setResizable(true);
-        primaryStage.setTitle("Titre de l'application");
-        Scene s = new Scene(new MainWindow());
-        s.getStylesheets().add("/fr/univ_amu/iut/applicationfx/Buttons.css");
-        primaryStage.setWidth(1200);
-        primaryStage.setHeight(700);
-        primaryStage.setScene(s);
-        primaryStage.show();
+    public void start(Stage stage) {
+
+        appBasis(stage);
+
+        VBox mainWindow = new MainWindow();
+        mainWindow.getChildren().add(new Home());
+        root = new Scene(mainWindow);
+        loadCSS();
+
+        stage.setScene(root);
+        stage.show();
+    }
+
+    private void appBasis(Stage stage){
+        stage.setResizable(false);
+        stage.setTitle("Titre de l'application");
+        stage.setWidth(1200);
+        stage.setHeight(700);
+    }
+
+    private void loadCSS(){
+        root.getStylesheets().add("/fr/univ_amu/iut/applicationfx/Buttons.css");
     }
 }
